848ac594967b3701fad06d0b8598b7a5a3b3e31c
Boaz Blake <BBlake@goempyrean.com>
2021-12-27 12:13:40 -0600

    build

--- app/views/child/index.js
+++ app/views/child/index.js
@@ -3,5 +3,5 @@ import m from "mithril"
 
 export default {
   oninit: (vnode) => (vnode.state.title = "CHILD PAGE"),
-  view: (v) => m(m.route.link, { selector: "h1", href: "/" }, "go home"),
+  view: (v) => m(m.route.Link, { selector: "h1", href: "/" }, "go home"),
 }
--- app/views/home/index.js
+++ app/views/home/index.js
@@ -4,5 +4,5 @@ import m from "mithril"
 export default {
   oninit: (v) => (v.state.title = "HOME PAGE"),
   view: () =>
-    m(m.route.link, { selector: "h1", href: "/child" }, "go to child"),
+    m(m.route.Link, { selector: "h1", href: "/child" }, "go to child"),
 }
