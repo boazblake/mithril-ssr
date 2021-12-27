d38de4fe30014152dbf80e84a50d55ce38cc0273
boaz <boazblake@gmail.com>
2021-12-27 08:25:54 -0600

    build

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ import domready from 'domready';
 import m from 'mithril';
 import routes   from './routes';
 
-m.route.prefix('');
+// m.route.prefix('');
 
 domready(function(){
   m.route(document.body, '/', routes);
