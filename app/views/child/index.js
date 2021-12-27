34aab782daaa8f7c81b4197d0982122d1ba0011c
Boaz Blake <BBlake@goempyrean.com>
2021-12-27 11:54:32 -0600

    build

--- app/views/child/index.js
+++ app/views/child/index.js
@@ -1,8 +1,7 @@
 "use strict"
-import layout from "../layout"
 import m from "mithril"
 
 export default {
   oninit: (vnode) => (vnode.state.title = "CHILD PAGE"),
-  view: (v) => m("a", { href: "/" }, "go home"),
+  view: (v) => m("a", { href: "mithril-ssr/#!/" }, "go home"),
 }
--- app/views/home/index.js
+++ app/views/home/index.js
@@ -1,8 +1,7 @@
 "use strict"
-import layout from "../layout"
 import m from "mithril"
 
 export default {
   oninit: (v) => (v.state.title = "HOME PAGE"),
-  view: () => m("a", { href: "/child" }, "go to child"),
+  view: () => m("a", { href: "mithril-ssr/#!/child" }, "go to child"),
 }
